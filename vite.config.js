f8c22a21e0d5bee97ef5e7160dea9878d24c109f
Pishta Oleksandr <live.gp@gmail.com>
2024-09-26 22:30:54 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
+import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import ImagePresetsPlugin, { widthPreset } from 'vite-plugin-image-presets';
+import ViteMinifyPlugin from 'vite-plugin-minify';
+import { reactClickToComponent } from 'vite-plugin-react-click-to-component';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    reactClickToComponent(),
+    ViteMinifyPlugin({}),
+    VitePluginSvgSpritemap('./src/icons/*.svg'),
+    ImagePresetsPlugin({
+      thumbnail: widthPreset({
+        class: 'img thumb',
+        loading: 'lazy',
+        widths: [48, 96],
+        formats: {
+          webp: { quality: 50 },
+          jpg: { quality: 70 },
+        },
+      }),
+    }),
+  ],
   base: '/vite-project/',
 });
