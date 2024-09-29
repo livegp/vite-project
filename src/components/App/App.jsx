d51f946e05fee51ad77a568253fabdcb6b9d1327
Pishta Oleksandr <live.gp@gmail.com>
2024-09-29 21:49:24 +0300

    add and del

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,42 @@
-import viteLogo from '/vite.svg';
+import { Global } from '@emotion/react';
 import { useState } from 'react';
-
+import viteLogo from '/vite.svg';
 import reactLogo from '../../assets/react.svg';
-
-import './App.css';
+import {
+  AnimatedLogo,
+  Button,
+  Card,
+  GlobalStyles,
+  Logo,
+  StyledLink,
+  Text,
+  Title,
+} from './App.styled';
 
 export function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Global styles={GlobalStyles} />
       <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} class='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} class='logo react' alt='React logo' />
-        </a>
+        <StyledLink href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+          <Logo src={viteLogo} alt='Vite logo' />
+        </StyledLink>
+        <StyledLink href='https://react.dev' target='_blank' rel='noreferrer'>
+          <AnimatedLogo src={reactLogo} alt='React logo' />
+        </StyledLink>
       </div>
-      <h1>Vite + React + Bun</h1>
-      <div class='card'>
-        <button type='button' onClick={() => setCount(count => count + 1)}>
+      <Title>Vite + React + Bun</Title>
+      <Card>
+        <Button type='button' onClick={() => setCount(count => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
-      <p class='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      </Card>
+      <Text>Click on the Vite and React logos to learn more</Text>
     </>
   );
 }
--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -1,5 +1,119 @@
+import { css, keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 
+export const Logo = styled.img`
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+  &:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+`;
+
+export const ReactLogo = styled(Logo)`
+  &:hover {
+    filter: drop-shadow(0 0 2em #61dafbaa);
+  }
+`;
+
+export const StyledLink = styled.a`
+  font-weight: 500;
+  color: #646cff;
+  text-decoration: inherit;
+  &:hover {
+    color: #535bf2;
+  }
+
+  @media (prefers-color-scheme: light) {
+  &:hover {
+    color: #747bff;
+  }
+}
+`;
+
+export const Title = styled.h1`
+  font-size: 3.2em;
+  line-height: 1.1;
+`;
+
 export const Button = styled.button`
-color: turquoise;
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #1a1a1a;
+  cursor: pointer;
+  transition: border-color 0.25s;
+  &:hover {
+    border-color: #646cff;
+  }
+  &:focus,
+  &:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+  }
+
+  @media (prefers-color-scheme: light) {
+    background-color: #f9f9f9;
+}
+`;
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const AnimatedLogo = styled(ReactLogo)`
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${spin} infinite 20s linear;
+  }
+`;
+
+export const Card = styled.div`
+  padding: 2em;
+`;
+
+export const Text = styled.p`
+  color: #888;
+`;
+
+export const GlobalStyles = css`
+:root {
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  line-height: 1.5;
+  font-weight: 400;
+  color-scheme: light dark;
+  color: rgba(255, 255, 255, 0.87);
+  background-color: #242424;
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  
+  @media (prefers-color-scheme: light) {
+    color: #213547;
+    background-color: #ffffff;
+  }
+}
+
+#root {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+}
+
+body {
+  margin: 0;
+  display: flex;
+  place-items: center;
+  min-width: 320px;
+  min-height: 100vh;
+  }
 `;
