079bebf50e1c3a6eb03fbfe80725dbae237d7dba
Pishta Oleksandr <live.gp@gmail.com>
2024-10-03 00:00:29 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,91 @@
-import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap';
 import react from '@vitejs/plugin-react-swc';
 import TurboConsole from 'unplugin-turbo-console/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import biomePlugin from 'vite-plugin-biome';
-import ImagePresetsPlugin, { widthPreset } from 'vite-plugin-image-presets';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 import { reactClickToComponent } from 'vite-plugin-react-click-to-component';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/vite-project',
-  esbuild: {
-    minify: true,
-    treeShaking: true,
-    drop: ['console', 'debugger'],
-  },
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      plugins: [['@swc/plugin-emotion', {}]],
-    }),
-    TurboConsole({}),
-    reactClickToComponent(),
-    VitePluginSvgSpritemap('./src/icons/*.svg'),
-    ImagePresetsPlugin({
-      thumbnail: widthPreset({
-        class: 'img thumb',
-        loading: 'lazy',
-        widths: [48, 96],
-        formats: {
-          webp: { quality: 50 },
-          jpg: { quality: 70 },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    define: {
+      'process.env': env,
+    },
+    base: env.VITE_BASE_URL || '/',
+    server: {
+      port: mode === 'development' ? 3000 : 8080,
+    },
+    build: {
+      outDir: mode === 'production' ? 'dist' : 'build',
+    },
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+        plugins: [['@swc/plugin-emotion', {}]],
+      }),
+      TurboConsole({}),
+      reactClickToComponent(),
+      vitePluginFaviconsInject('./src/assets/favicons/logo.svg', {
+        config: {},
+        pluginConfig: {
+          failGraciously: false,
         },
       }),
-    }),
-    biomePlugin({
-      mode: 'check',
-      files: '.',
-      applyFixes: true,
-      failOnError: false,
-    }),
-  ],
+      // vitePluginFaviconsInject('./src/assets/favicons/logo.svg', {
+      //   // path: '/',
+      //   // appName: 'vite-project',
+      //   // appShortName: 'vite-project',
+      //   // appDescription: 'starting template',
+      //   // developerName: 'Pishta Oleksandr',
+      //   // // biome-ignore lint/style/useNamingConvention: <explanation>
+      //   // developerURL: 'https://livegp.github.io',
+      //   // cacheBustingQueryParam: null,
+      //   // dir: 'auto',
+      //   // lang: 'en-US',
+      //   // background: '#fff',
+      //   // // biome-ignore lint/style/useNamingConvention: <explanation>
+      //   // theme_color: '#fff',
+      //   // appleStatusBarStyle: 'black-translucent',
+      //   // display: 'standalone',
+      //   // orientation: 'any',
+      //   // scope: '/',
+      //   // // biome-ignore lint/style/useNamingConvention: <explanation>
+      //   // start_url: '/?homescreen=1',
+      //   // preferRelatedApplications: false,
+      //   // relatedApplications: undefined,
+      //   // version: '1.0',
+      //   // // biome-ignore lint/style/useNamingConvention: <explanation>
+      //   // pixel_art: false,
+      //   // loadManifestWithCredentials: false,
+      //   // manifestMaskable: false,
+      //   icons: {
+      //     android: true,
+      //     appleIcon: true,
+      //     appleStartup: true,
+      //     favicons: true,
+      //     windows: true,
+      //     yandex: false,
+      //   },
+      // }),
+      // ImagePresetsPlugin({
+      //   thumbnail: widthPreset({
+      //     class: 'img thumb',
+      //     loading: 'lazy',
+      //     widths: [48, 96],
+      //     formats: {
+      //       webp: { quality: 50 },
+      //       jpg: { quality: 70 },
+      //     },
+      //   }),
+      // }),
+      biomePlugin({
+        mode: 'check',
+        files: '.',
+        applyFixes: true,
+        failOnError: false,
+      }),
+    ],
+  };
 });
